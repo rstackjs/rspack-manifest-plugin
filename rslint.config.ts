58c3c5436a9e20fef1555b4b1e34c584b87c401f
neverland <chenjiahan.jait@bytedance.com>
2026-05-07 21:24:46 +0800

    chore: add Rslint

---
+++ rslint.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig, js, ts } from '@rslint/core';
+
+export default defineConfig([
+  js.configs.recommended,
+  ts.configs.recommended,
+  {
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'off',
+    },
+  },
+]);
