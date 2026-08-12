d432615eb2d69cbac150fa4d369e73ac7acef0ab
Jiahan Chen <chenjiahan.jait@bytedance.com>
2026-08-12 22:01:22 +0800

    chore(deps): upgrade Rslint to v0.8.0 (#24)

--- rslint.config.ts
+++ rslint.config.ts
@@ -8,4 +8,12 @@ export default defineConfig([
       '@typescript-eslint/no-explicit-any': 'off',
     },
   },
+  {
+    files: ['.prettierrc.js', 'test/**/*'],
+    rules: {
+      '@typescript-eslint/no-require-imports': 'off',
+      '@typescript-eslint/no-unused-expressions': 'off',
+      'no-undef': 'off',
+    },
+  },
 ]);
